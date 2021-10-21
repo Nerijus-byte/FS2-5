c7bbabf7ab1bb3cbcc5555a4f23311bff9570b94
Nerijus-byte <82173726+Nerijus-byte@users.noreply.github.com>
2021-10-21 18:40:31 +0300

    Version TWO

--- 2021-10-20/Classwork.js
+++ 2021-10-20/Classwork.js
@@ -15,16 +15,26 @@
 //     Paskutinę įvestį (<input>) į formą.
 
 let age_table = document.getElementById("age-table");
+let age_tableV2 = document.querySelector('#age-table');
     console.log(age_table);
+    console.log(age_tableV2);
 
 let age = document.getElementById("age");
+let ageV2 = document.querySelector("#age");
     console.log(age);
+    console.log(ageV2);
 
 let label_elements = document.getElementsByTagName("label");
+let label_elementsV2 = document.querySelectorAll("label");
     console.log(label_elements);
+    console.log(label_elementsV2);
 
 let name = document.getElementById("fname");
+let nameV2 = document.querySelectorAll("input:first-of-type")
     console.log(name);
+    console.log(nameV2);
 
 let input_age = document.getElementById("ageof");
+let input_ageV2 = document.querySelectorAll("input:last-of-type");
     console.log(input_age);
+    console.log(input_ageV2);
