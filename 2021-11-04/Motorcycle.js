f840e1dd985c412f335a97ece013649e4e821ea7
Nerijus-byte <82173726+Nerijus-byte@users.noreply.github.com>
2021-11-04 20:07:33 +0200

    Motorcycle

---
+++ 2021-11-04/Motorcycle.js
@@ -0,0 +1,13 @@
+class Motorcycle extends Car{
+    constructor(make, model, year, wheels) {
+        super(make, model, year);
+        this.wheels = wheels;
+    }
+    getWheelsNumber(){
+        if (this.wheels === 3) {
+            return 'motociklas turi 3 ratus';
+        } else if (this.wheels === 2) {
+            return 'motociklas turi 2 ratus';
+        }
+    }
+}
